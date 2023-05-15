9e3cb98ee72f94fbdcd8e7068add80f99e4a56ef
gittjar <112698736+gittjar@users.noreply.github.com>
2023-05-15 21:30:01 +0300

    Create citybikestations.sql

---
+++ citybikestations.sql
@@ -0,0 +1,15 @@
+CREATE TABLE citybikestations (
+    FID int,
+    ID int,
+    Nimi varchar(100),
+    Namn varchar(100),
+    Name varchar(100),
+    Osoite varchar(100),
+    Adress varchar(100),
+    Kaupunki varchar(100),
+    Stad varchar(100),
+    Operaattor varchar(100),
+    Kapasiteet int(3),
+    x decimal(8,6),
+    y decimal(9,6)
+);
