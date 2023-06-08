b8c5bf60c878975dc4dcc9564b0fb2d047e1d425
gittjar <112698736+gittjar@users.noreply.github.com>
2023-06-08 08:40:53 +0300

    Update SQL-AzureDB-testdata.sql

--- SQL-AzureDB-testdata.sql
+++ SQL-AzureDB-testdata.sql
@@ -70,3 +70,12 @@ SELECT    roles.principal_id                            AS RolePrincipalID
  JOIN sys.database_principals AS members  
   ON database_role_members.member_principal_id = members.principal_id;  
  GO
+
+-- 
+-- Show users all priviledges
+--
+SELECT DISTINCT pr.principal_id, pr.name, pr.type_desc, 
+    pr.authentication_type_desc, pe.state_desc, pe.permission_name
+FROM sys.database_principals AS pr
+JOIN sys.database_permissions AS pe
+    ON pe.grantee_principal_id = pr.principal_id;
